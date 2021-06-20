f9156c0e2660b493fa17851a22b1a467581514c6
Raymond Feng <rf.raymondfeng@gmail.com>
2021-06-20 20:43:31 +1200

    Create transactions repository

--- main/service/mongo_service.py
+++ main/service/mongo_service.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
+from pymongo.collection import Collection
 
 
 class MongoService:
 
     def __init__(self, uri, db_name):
-        self.client = MongoClient(uri)
-        self.database = self.client[db_name]
+        self.__client = MongoClient(uri)
+        self.__database = self.__client[db_name]
 
         print("[Server] Mongo service initiated")
+
+    def collection(self, collection_name: str) -> Collection:
+        return self.__database[collection_name]
