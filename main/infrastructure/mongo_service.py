09b30d2ac9e59834db678c6b4b6854eb49de4d6b
Raymond Feng <rf.raymondfeng@gmail.com>
2021-06-22 00:51:44 +1200

    Improve MongoService commenting and move initialisation to service

--- main/infrastructure/mongo_service.py
+++ main/infrastructure/mongo_service.py
@@ -3,9 +3,18 @@ from pymongo.collection import Collection
 
 
 class MongoService:
+    """
+    Wrapper for interacting with MongoDB.
+    """
 
     def __init__(self, uri, db_name):
-        self.__client = MongoClient(uri)
+        """
+        Connecting to a Mongo Atlas cluster.
+
+        :param uri: Mongo atlas uri with credentials.
+        :param db_name: The name of the database to connect to.
+        """
+        self.__client = MongoClient(uri, ssl=True, tlsAllowInvalidCertificates=True)
         self.__database = self.__client[db_name]
 
         print("[Server] Mongo service initiated")
