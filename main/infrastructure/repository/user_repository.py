9101b903d4b78c4f2463b66950bce47278e0b76c
Raymond Feng <rf.raymondfeng@gmail.com>
2021-06-28 16:59:07 +1200

    Add UserRepository.get_user_by_id()

--- main/infrastructure/repository/user_repository.py
+++ main/infrastructure/repository/user_repository.py
@@ -14,10 +14,10 @@ class UserRepository(Repository):
 
     def __init__(self,
                  mongo_service: MongoService,
-                 account_repository: AccountRepository):
+                 account_repo: AccountRepository):
         self.__user_collection = mongo_service.collection("users")
         self.__user_mapper = UserEntityMapper()
-        self.__account_repository = account_repository
+        self.__account_repo = account_repo
 
     def list(self) -> List[User]:
         pipeline = [{
@@ -32,11 +32,20 @@ class UserRepository(Repository):
         dbo_list = self.__user_collection.aggregate(pipeline)
         return [self.__user_mapper.to_domain_model(dbo) for dbo in dbo_list]
 
+    def get_user_by_id(self, id: str):
+        """
+        Gets a user by their `id` and returns the user. If no user with `id` is found, then return `None`.
+        """
+
+        dbo = self.__user_collection.find_one({"_id": id})
+
+        return self.__user_mapper.to_domain_model(dbo) if dbo else None
+
     def save(self, user: User) -> User:
         dbo = self.__user_mapper.from_domain_model(user)
 
         # Save all accounts to the DB
-        [self.__account_repository.save(account) for account in user.accounts]
+        [self.__account_repo.save(account) for account in user.accounts]
 
         if user.id:
             result = self.__user_collection.update_one({'_id': ObjectId(user.id)}, {'$set': dbo}, upsert=True)
